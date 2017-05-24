ab0e99e6eb1f634cb368536af9a0a769325588fc
JaeLee18 <lee2161@purdue.edu>
2017-05-24 21:59:00 +0900

    Node

---
+++ underscoreTest.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jaejoonglee on 5/24/17.
+ */
+var _ =  require('underscore');
+var arr = [3, 6, 9, 1, 12];
+console.log(arr[0]);
+console.log(_.first(arr));
+console.log(_.last(arr));
